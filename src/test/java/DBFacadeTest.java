7ee388979597c5f16977a3157e91881436b26424
thejamiegc <jcallan32@gmail.com>
2023-02-01 15:27:59 +0100

    updates test 1

--- src/test/java/DBFacadeTest.java
+++ src/test/java/DBFacadeTest.java
@@ -46,7 +46,7 @@ class DBFacadeTest {
 
     @Test
     public void test() throws SQLException {
-        System.out.println("testning Database Connection");
+        System.out.println("testning Database Connection, to see first name");
         String sql = "SELECT fname FROM startcode_test.usertable";
         try(ResultSet set = con.prepareStatement(sql).executeQuery()){
            set.next();
